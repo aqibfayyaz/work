1cd3082e4310d646851ef03cd1f19f2bbd5a536b
Aqib <aqibbfayyaz@gmail.com>
2021-10-22 20:35:22 +0500

    first flow

--- flow/flow.py
+++ flow/flow.py
@@ -5,7 +5,7 @@ from prefect.run_configs import KubernetesRun
 
 FLOW_NAME = "flow"
 STORAGE = GitHub(
-    repo="aqibfayyaz/InternshipWork",
+    repo="aqibfayyaz/work",
     path=f"flows/{FLOW_NAME}.py",
     access_token_secret="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",  # required with private repositories
 )
